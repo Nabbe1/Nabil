5afb529f6864455795309c8a8f0a2a59075ff2d4
Nabbe1 <61581670+Nabbe1@users.noreply.github.com>
2020-05-12 13:49:55 +0200

    Add files via upload

---
+++ AreaOfAnCircle/AreaOfAnCircle/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AreaOfAnCircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int r;
+            double A;
+            Console.WriteLine("Skriv in radien av circlen:");
+            r = Convert.ToInt32(Console.ReadLine());
+            A = (3.14) * r * r;
+            Console.WriteLine("Arean av cirkel är=" + A);
+            Console.ReadLine();
+        }
+    }
+}
